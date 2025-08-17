3e94693a10f4587b67572c7b2a91337502003dc6
Tanmay <n12307700@qut.edu.au>
2025-08-17 13:43:25 +1000

    public ip change

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://16.176.12.137:5001', // live
+  baseURL: 'http://3.25.111.55:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
